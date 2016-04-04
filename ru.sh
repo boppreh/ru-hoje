72888e73f7aae9d6401f8bead4cec4709a58d79d
BoppreH <boppreh@gmail.com>
2016-04-04 17:40:11 -0300

    Rewrite script to single pipe.

--- ru.sh
+++ ru.sh
@@ -1,23 +1,11 @@
-# copyright Matteus Legat 2016
-#
-# script que mostra o cardápio do RU UFSC de hoje
-#
+# Fetch HTML page with calendar and menu.
+curl --compressed http://ru.ufsc.br/ru/ 2>/dev/null |
 
-declare html=$(curl -s --compressed http://ru.ufsc.br/ru/)
-
-
-# mostra somente dia atual se não for passado o parâmetro `all`
-[ "$1" != "all" ] && [ "$1" == "" ] && declare weekday=`LANG=pt_BR.UTF-8 date +%a`
-
-# mostra data passada por parâmetro
-[ "$1" != "all" ] && [ "$1" != "" ] && declare weekday=$1
-
-
-# pega somente o conteúdo da tabela do cardápio
-declare menu_itself=$(echo -e "$html" | sed -n '/Sobremesa/,/Ingredientes/p' | sed -e '1d;$d')
-
-
-                          #pega apenas dia da semana          # remove tags      # remove linhas e inicios em branco
-echo -e "$menu_itself" | sed -n "/${weekday^} */,/<\/tr>/p" | sed -e 's/<[^>]*>//g;/^ *[&nbsp;]*$/d;s/^[ * *\t]*//'
-
-
+  # Take table row corresponding to day of week (Mon=2,Tue=3,Sun=8).
+  awk "/<tr>/{i++}i==$(date +%u)+1" | 
+  
+  # Remove HTML tags.
+  sed 's/<\/*[^>]*>//g' |
+  
+  # Ignore leading spaces (\xC2\xA0 is non-breaking space).
+  grep -Po '[^\s\xc2\xa0].+'
